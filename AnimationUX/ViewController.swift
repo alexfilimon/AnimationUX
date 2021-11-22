eb435506dbb6271024a610ca7ede88cbf7662847
Aleksandr Filimonov <as_filimon@mai.ru>
2021-11-22 08:32:43 +0200

    Initial Commit

---
+++ AnimationUX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationUX
+//
+//  Created by al.filimonov on 22.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
